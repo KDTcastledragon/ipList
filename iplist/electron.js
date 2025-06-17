feca33605740ff956a9f71901837e93c35f003f5
이성룡\Daelim <starlight1532@gmail.com>
2025-06-17 15:37:14 +0900

    드디어 성공!!!!

--- iplist/electron.js
+++ iplist/electron.js
@@ -1,7 +1,12 @@
-// const { app, BrowserWindow } = require('electron');
-const isDev = require('electron-is-dev');
+const { app, BrowserWindow } = require('electron');
+const { dialog } = require('electron');
+// const isDev = require('electron-is-dev');
 
 function createWindow() {
+  console.log(`제대로 작동중인가요???`);
+    console.log(`제대로 작동중인가요???`);
+    console.log(`제대로 작동중인가요???`);
+
   const win = new BrowserWindow({
     width: 1024,
     height: 768,
@@ -12,14 +17,26 @@ function createWindow() {
     },
   });
 
+const isDev = !app.isPackaged;
 
   if (isDev) {
+     console.error(`페이지 로드 실패: ${errorDescription} (${errorCode}) at ${validatedURL}`);
     win.loadURL('http://localhost:3000');
   } else {
-    win.loadFile('./public/index.html');
+    win.loadFile('./build/index.html');
   }
 
-  win.webContents.openDevTools();
+  // win.webContents.openDevTools();
+  win.webContents.on('did-fail-load', (event, errorCode, errorDescription, validatedURL) => {
+    dialog.showErrorBox('페이지 로드 실패', `오류 코드: ${errorCode}\n설명: ${errorDescription}`);
+});
 }
 
 app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
