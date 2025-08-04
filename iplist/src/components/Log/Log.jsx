9430b35e9f65ce821ecb0b2b500d0db9ecdddbc3
castledragon <starlight1532@gmail.com>
2025-08-05 01:19:35 +0900

    dd

--- iplist/src/components/Log/Log.jsx
+++ iplist/src/components/Log/Log.jsx
@@ -6,29 +6,7 @@ function Log() {
 
     return (
         <>
-            <h1>변동 기록</h1>
-            <div>
-                <span>관리번호 : </span>
-                <span>h2134k</span>
-            </div>
-            <div>
-                <span>분류</span>
-                <hr />
-                <select name="d" id="">
-                    <option value="deskTop">데스크탑</option>
-                    <option value="noteBook">노트북</option>
-                    <option value="workStation">WORKSTATION</option>
-                    <option value="PDA">PDA</option>
-                </select>
-            </div>
-            <div>사번 : <span>8468464535</span></div>
-            <div>이름 : <span>이성룡</span></div>
-            <div>직급 : <span>인턴</span></div>
-            <div>소속 : <span>IT팀</span></div>
-            <div>IP :
-                <input type="text" value={"192.168.456.866"} readOnly />
-            </div>
-            <button onClick={() => alert(`수정버튼`)}>수정버튼</button>
+            <h1>USB</h1>
         </>
     );
 }
