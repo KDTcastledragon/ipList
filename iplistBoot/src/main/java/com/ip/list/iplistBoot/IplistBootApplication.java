a0b739a3928558a3722e01af985ad8ab0a635e19
castledragon <starlight1532@gmail.com>
2025-08-11 23:06:27 +0900

    springboot maven java17

---
+++ iplistBoot/src/main/java/com/ip/list/iplistBoot/IplistBootApplication.java
@@ -0,0 +1,13 @@
+package com.ip.list.iplistBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IplistBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IplistBootApplication.class, args);
+	}
+
+}
---
+++ iplistBoot/src/test/java/com/ip/list/iplistBoot/IplistBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ip.list.iplistBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IplistBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
